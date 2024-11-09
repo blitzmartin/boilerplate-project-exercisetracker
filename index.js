ec3d5e2164c2deb304186c236618654e01fc38cf
annalisa4sec <122273976+annalisa4sec@users.noreply.github.com>
2024-11-09 19:30:14 +0100

    add missing library and connect to db

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+let mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(cors())
 app.use(express.static('public'))
